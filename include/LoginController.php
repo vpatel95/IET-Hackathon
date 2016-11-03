0642550ff5d4374a4402dc44b90449b265580d29
Ved Patel <vedmpatel@gmail.com>
2016-11-04 02:13:16 +0530

    Added login and signup

---
+++ include/LoginController.php
@@ -0,0 +1,48 @@
+<?php
+
+    /**
+     * Class to handle all db operations
+     * This class will have CRUD methods for database tables
+     *
+     * @author Ved Patel
+     */
+    class LoginController {
+
+        private $conn;
+
+        function __construct() {
+            require_once dirname(__FILE__) . '/DbConnect.php';
+            // opening db connection
+            $db = new DbConnect();
+            $this->conn = $db->connect();
+            mysqli_autocommit($conn, FALSE);
+        }
+
+        public function login($username, $password) {
+            if($stmt = mysqli_prepare($conn, "SELECT id FROM user WHERE username=? AND password=?")){
+                mysqli_stmt_bind_param($stmt, 'ss', $username, $password);
+                mysqli_stmt_execute($stmt);
+                $result = mysqli_stmt_get_result($stmt);
+                $row = mysqli_fetch_assoc($result);
+                $count = mysqli_stmt_num_rows($stmt);
+                mysqli_stmt_close($stmt);
+            }
+
+            if($count == 1) {
+                return 1;
+            } else {
+                return 0;
+            }
+            
+        }
+
+        public function signup($username, $password) {
+            if($stmt = mysqli_prepare($conn, "INSERT INTO user VALUES (?,?)")){
+                mysqli_stmt_bind_param($stmt, 'ss', $username, $password);
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_close($stmt);
+            }
+        }
+
+    }
+?>
